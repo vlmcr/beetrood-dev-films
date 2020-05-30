842a70990f15f9f77f2b0001b63efe2dbcbe5ba2
Vadym Dobrydnyk <vdobrydnyk@lohika.com>
2020-05-30 12:49:39 +0300

    Refactor signup form

--- client/src/components/forms/SignupForm.js
+++ client/src/components/forms/SignupForm.js
@@ -1,7 +1,8 @@
-import React from "react"
+import React, {useState} from "react"
 import {Link} from "react-router-dom"
 import FormMessage from "./FormMessage"
 import isEmail from "validator/lib/isEmail"
+import setFormObj from "./FormUtils"
 
 const initialData = {
   email: "",
@@ -9,33 +10,12 @@ const initialData = {
   passwordConfirmation: "",
 }
 
-class SignupForm extends React.Component {
-  state = {
-    data: initialData,
-    errors: {},
-    loading: false,
-  }
-  handleChange = e =>
-    this.setState({
-      data: {...this.state.data, [e.target.name]: e.target.value},
-       errors: {...this.state.errors, [e.target.name]: ""},
-    })
+const SignupForm = props => {
+  const [form, setForm] = useState(initialData);
+  const [errors, setErrors] = useState({});
+  const [loading, setLoading] = useState(false);
 
-  handleSubmit = e => {
-    e.preventDefault()
-    const errors = this.validate(this.state.data)
-    this.setState({errors})
-    if (Object.keys(errors).length === 0) {
-      this.setState({loading: true})
-      this.props
-        .submit(this.state.data)
-        .catch(error =>
-          this.setState({errors: error.response.data.errors, loading: false}),
-        )
-    }
-  }
-
-  validate(data) {
+  const validate = (data) => {
     const errors = {}
     if (!isEmail(data.email)) errors.email = "Wrong format email"
     if (!data.email) errors.email = "Email cannot be blank"
@@ -45,62 +25,73 @@ class SignupForm extends React.Component {
     return errors
   }
 
-  render() {
-    const {data, errors, loading} = this.state
-    const cls = loading ? "ui form loading" : "ui form"
-    return (
-      <form className={cls} onSubmit={this.handleSubmit}>
-        <div className={errors.email ? "error field" : "field"}>
-          <label>Email</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Email"
-            value={data.email}
-            onChange={this.handleChange}
-          />
-          <FormMessage>{errors.email}</FormMessage>
-        </div>
-
-        <div className={errors.password ? "error field" : "field"}>
-          <label>Password</label>
-          <input
-            type="text"
-            name="password"
-            id="password"
-            placeholder="password"
-            value={data.password}
-            onChange={this.handleChange}
-          />
-          <FormMessage>{errors.password}</FormMessage>
-        </div>
-
-        <div className={errors.passwordConfirmation ? "error field" : "field"}>
-          <label>Password Confirmation</label>
-          <input
-            type="text"
-            name="passwordConfirmation"
-            id="passwordConfirmation"
-            placeholder="password confirmation"
-            value={data.passwordConfirmation}
-            onChange={this.handleChange}
-          />
-          <FormMessage>{errors.passwordConfirmation}</FormMessage>
-        </div>
-
-        <div className="ui fluid buttons">
-          <button className="ui button primary">Sing Up</button>
-
-          <div className="or" />
-
-          <Link to="/" className="ui button">
-            Cancel
-          </Link>
-        </div>
-      </form>
-    )
+  const handleSubmit = e => {
+    e.preventDefault()
+    const errors = validate(form)
+    setErrors(errors)
+    if (Object.keys(errors).length === 0) {
+      setLoading(true)
+      props.submit(form).catch(error => {
+        setErrors(error.response.data.errors)
+        setLoading(false)
+      })
+    }
   }
+
+  const cls = loading ? "ui form loading" : "ui form"
+
+  return (
+    <form className={cls} onSubmit={handleSubmit}>
+      <div className={errors.email ? "error field" : "field"}>
+        <label>Email</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email"
+          value={form.email}
+          onChange={setFormObj(form, setForm)}
+        />
+        <FormMessage>{errors.email}</FormMessage>
+      </div>
+
+      <div className={errors.password ? "error field" : "field"}>
+        <label>Password</label>
+        <input
+          type="text"
+          name="password"
+          id="password"
+          placeholder="password"
+          value={form.password}
+          onChange={setFormObj(form, setForm)}
+        />
+        <FormMessage>{errors.password}</FormMessage>
+      </div>
+
+      <div className={errors.passwordConfirmation ? "error field" : "field"}>
+        <label>Password Confirmation</label>
+        <input
+          type="text"
+          name="passwordConfirmation"
+          id="passwordConfirmation"
+          placeholder="password confirmation"
+          value={form.passwordConfirmation}
+          onChange={setFormObj(form, setForm)}
+        />
+        <FormMessage>{errors.passwordConfirmation}</FormMessage>
+      </div>
+
+      <div className="ui fluid buttons">
+        <button className="ui button primary">Sing Up</button>
+
+        <div className="or" />
+
+        <Link to="/" className="ui button">
+          Cancel
+        </Link>
+      </div>
+    </form>
+  )
 }
 
 export default SignupForm
