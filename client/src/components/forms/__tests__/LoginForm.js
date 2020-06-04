e0106ce8f44003c424d852199f03beecbab2cd98
Vadym Dobrydnyk <vdobrydnyk@lohika.com>
2020-06-04 21:23:38 +0300

    Use jest-dom functions

--- client/src/components/forms/__tests__/LoginForm.js
+++ client/src/components/forms/__tests__/LoginForm.js
@@ -2,6 +2,9 @@ import React from 'react';
 import LoginForm from '../LoginForm';
 import ReactDOM from 'react-dom';
 import { MemoryRouter } from "react-router-dom";
+import { toHaveAttribute, toHaveTextContent} from "@testing-library/jest-dom/matchers"
+
+expect.extend({toHaveAttribute, toHaveTextContent})
 
 test("LoginForm should render correct", () => {
   const div = document.createElement("div");
@@ -12,5 +15,7 @@ test("LoginForm should render correct", () => {
     div
   );
 
-  expect(div.querySelector("#email").type).toBe("email")
+  // expect(div.querySelector("#email").type).toBe("email")
+  expect(div.querySelector("#email")).toHaveAttribute("type", "email");
+  expect(div.querySelector("label")).toHaveTextContent("Email");
 })
