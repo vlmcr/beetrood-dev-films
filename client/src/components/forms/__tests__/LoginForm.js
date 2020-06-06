befb46226d9889917527aeff19f638d9b1452b57
Vadym Dobrydnyk <vdobrydnyk@lohika.com>
2020-06-06 12:05:46 +0300

    Use user.type library

--- client/src/components/forms/__tests__/LoginForm.js
+++ client/src/components/forms/__tests__/LoginForm.js
@@ -3,6 +3,7 @@ import LoginForm from '../LoginForm';
 import { MemoryRouter } from "react-router-dom";
 import { toHaveAttribute, toHaveTextContent} from "@testing-library/jest-dom/matchers"
 import { render, fireEvent } from "@testing-library/react"
+import user from "@testing-library/user-event";
 
 expect.extend({toHaveAttribute, toHaveTextContent})
 
@@ -11,39 +12,57 @@ const submit = jest.fn(() => Promise.resolve())
 const data = {
   email: "test@mail.com",
   password: "mypass",
-}
+};
 
-test("LoginForm check email input", () => {
-  const { getByLabelText } = render(
-    <MemoryRouter>
-      <LoginForm />
-    </MemoryRouter>
-  );
-  const emailEl = getByLabelText(/email/i)
-  expect(emailEl).toHaveAttribute("type", "email");
-})
+// test("LoginForm check email input", () => {
+//   const { getByLabelText } = render(
+//     <MemoryRouter>
+//       <LoginForm />
+//     </MemoryRouter>
+//   );
+//   const emailEl = getByLabelText(/email/i)
+//   expect(emailEl).toHaveAttribute("type", "email");
+// })
+//
+// test("Debug", () => {
+//   const { debug } = render(
+//     <MemoryRouter>
+//       <LoginForm />
+//     </MemoryRouter>
+//   );
+//   debug()
+// })
+//
+// test("LoginForm test snapshot", () => {
+//   const { container } = render(
+//     <MemoryRouter>
+//       <LoginForm />
+//     </MemoryRouter>
+//   );
+//
+//   expect(container.firstChild).toMatchSnapshot()
+// })
+//
+// test("LoginForm test submit (fireEvent)", () => {
+//   const { getByLabelText, getByTestId } = render(
+//     <MemoryRouter>
+//       <LoginForm submit={submit}/>
+//     </MemoryRouter>
+//   );
+//
+//   const emailElement = getByLabelText(/email/i);
+//   const passwordElement = getByLabelText(/password/i);
+//   fireEvent.change(emailElement, {target: {value: data.email}})
+//   fireEvent.change(passwordElement, {target: {value: data.password}})
+//
+//   const loginButton = getByTestId("login-button")
+//   fireEvent.click(loginButton)
+//   expect(submit).toHaveBeenCalledTimes(1);
+//   expect(submit).toHaveBeenCalledWith(data)
+// })
 
-test("Debug", () => {
-  const { debug } = render(
-    <MemoryRouter>
-      <LoginForm />
-    </MemoryRouter>
-  );
-  debug()
-})
-
-test("LoginForm test snapshot", () => {
-  const { container } = render(
-    <MemoryRouter>
-      <LoginForm />
-    </MemoryRouter>
-  );
-
-  expect(container.firstChild).toMatchSnapshot()
-})
-
-test("LoginForm should render correct", () => {
-  const { debug, getByLabelText, getByTestId } = render(
+test("LoginForm test submit (user event)", async () => {
+  const { getByLabelText, getByTestId } = render(
     <MemoryRouter>
       <LoginForm submit={submit}/>
     </MemoryRouter>
@@ -51,11 +70,11 @@ test("LoginForm should render correct", () => {
 
   const emailElement = getByLabelText(/email/i);
   const passwordElement = getByLabelText(/password/i);
-  fireEvent.change(emailElement, {target: {value: data.email}})
-  fireEvent.change(passwordElement, {target: {value: data.password}})
+  const loginButton = getByTestId("login-button");
 
-  const loginButton = getByTestId("login-button")
-  fireEvent.click(loginButton)
+  user.type(emailElement, data.email);
+  user.type(passwordElement, data.password);
+
+  fireEvent.click(loginButton);
   expect(submit).toHaveBeenCalledTimes(1);
-  expect(submit).toHaveBeenCalledWith(data)
 })
